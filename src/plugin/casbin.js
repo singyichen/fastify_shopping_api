52ed2ddd1d9d872a7952cf8ac992a62472b21441
陳欣怡 <mandy.chen@hokia.com.tw>
2023-09-14 08:06:16 +0800

    feat：add onDeny in fastifyCasbinRest in casbin.js
    原因：新增 onDeny 監聽在無訪問權限時回傳自定義錯誤訊息，並將錯誤訊息寫入 errorLogger 中
    
    調整項目：
    1. casbin.js：
        - 新增 onDeny 監聽
        - 新增 permissionDeniedInfo
        - 新增 將無訪問權限資訊寫入 errorLogger

--- src/plugin/casbin.js
+++ src/plugin/casbin.js
@@ -8,7 +8,8 @@ const fastifyCasbinRest = require('fastify-casbin-rest');
 const { join } = require('path');
 const { PrismaAdapter } = require('casbin-prisma-adapter');
 const prismaClientService = require('../ormService/prismaClientService');
-
+const { permissionDeniedInfo } = require('../utils/errorInfo');
+const errorLogger = require('../plugin/logger');
 async function casbinConnector(fastify, opts, done) {
   const model = join(__dirname, '../casbin/rbac', 'rbac_model.conf');
   const adapter = await PrismaAdapter.newAdapter(prismaClientService.prisma);
@@ -17,8 +18,18 @@ async function casbinConnector(fastify, opts, done) {
     adapter: adapter, // the adapter
   };
   fastify.register(fastifyCasbin, options);
-  fastify.register(fastifyCasbinRest);
-
+  fastify.register(fastifyCasbinRest, {
+    onDeny: (reply, { sub, obj, act }) => {
+      const message = `${sub} 無 ${act} ${obj} 權限`;
+      reply.code(403).send({
+        statusCode: permissionDeniedInfo.statusCode,
+        code: permissionDeniedInfo.code,
+        error: permissionDeniedInfo.error,
+        message: message,
+      });
+      errorLogger.error(message);
+    },
+  });
   done();
 }
 
