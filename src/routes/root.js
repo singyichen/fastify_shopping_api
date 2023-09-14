bedc50a53d5cb5b46b0576dbd043de7adb12189e
陳欣怡 <mandy.chen@hokia.com.tw>
2023-09-14 08:05:02 +0800

    feat：add casbin rest in router in root.js
    原因：新增 router 設定檢核權限邏輯
    
    調整項目：
    1. root.js：
        - getSub：取得角色
        - getObj：取得資源
        - getAct：取得操作

--- src/routes/root.js
+++ src/routes/root.js
@@ -4,18 +4,23 @@
  */
 const errorLogger = require('../plugin/logger');
 async function router(fastify, opts) {
-  fastify.get('/', async function (request, reply) {
-    try {
-      const isAllowed = await fastify.casbin.enforce('11548', 'data1', 'read');
-      if (!isAllowed) {
-        reply.send(new Error('Forbidden'));
+  fastify.get('/', {
+    casbin: {
+      rest: {
+        getSub: (request) => request.query.user_id,
+        getObj: 'data1',
+        getAct: 'read',
+      },
+    },
+    handler: async (request, reply) => {
+      try {
+        reply.send({ hello: 'world' });
+      } catch (error) {
+        errorLogger.error(error);
+        reply.send({ status: -1, message: error.message });
+        console.log(error);
       }
-      reply.send({ hello: 'world' });
-    } catch (error) {
-      errorLogger.error(error);
-      reply.send({ status: -1, message: error.message });
-      console.log(error);
-    }
+    },
   });
 }
 
