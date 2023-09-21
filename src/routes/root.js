752267228652c7f0fc4cd31ea7ca7eb723acf6ed
陳欣怡 <mandy.chen@hokia.com.tw>
2023-09-21 13:42:51 +0800

    feat：add router for test abac and rbac in root.js
    原因：新增測試 abac 與 rbac 的 router
    
    調整項目：
    1. root.js：
        - 新增 測試 abac router
        - 新增 測試 rbac router

--- src/routes/root.js
+++ src/routes/root.js
@@ -3,8 +3,21 @@
  * @description root API 路由
  */
 const errorLogger = require('../plugin/logger');
+const enforcerCreator = require('../plugin/enforcer');
+const { permissionDeniedInfo } = require('../utils/errorInfo');
 async function router(fastify, opts) {
   fastify.get('/', {
+    handler: async (request, reply) => {
+      try {
+        reply.send({ hello: 'world' });
+      } catch (error) {
+        errorLogger.error(error);
+        reply.send({ status: -1, message: error.message });
+        console.log(error);
+      }
+    },
+  });
+  fastify.get('/rbac', {
     casbin: {
       rest: {
         getSub: (request) => request.query.user_id,
@@ -15,6 +28,30 @@ async function router(fastify, opts) {
     handler: async (request, reply) => {
       try {
         reply.send({ hello: 'world' });
+      } catch (error) {
+        errorLogger.error(error);
+        reply.send({ status: -1, message: error.message });
+        console.log(error);
+      }
+    },
+  });
+  fastify.get('/abac', {
+    handler: async (request, reply) => {
+      try {
+        const enforcer = await enforcerCreator();
+        const query = request.query;
+        const p = await enforcer.enforce(query, '/data1', 'read');
+        if (!p) {
+          const age = query.Age;
+          const message = `Age ${age} 無 /data1 read 權限`;
+          reply.code(403).send({
+            statusCode: permissionDeniedInfo.statusCode,
+            code: permissionDeniedInfo.code,
+            error: permissionDeniedInfo.error,
+            message: message,
+          });
+        }
+        reply.send({ hello: 'abac' });
       } catch (error) {
         errorLogger.error(error);
         reply.send({ status: -1, message: error.message });
